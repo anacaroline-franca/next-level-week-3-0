98d6baa61be1768fd0cdc94072c7baa9a29aa156
Ana França <anacaroline.franca02@gmail.com>
2020-10-12 19:10:53 -0300

    [web] Add OrphanageMap static page

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import mapMarkerImg from '../images/local.svg'
+
+import '../styles/pages/orphanage.css';
+import  'leaflet/dist/leaflet.css';
 
 function OrphanagesMap() {
-    return <h1>Hello</h1>;
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy logo"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Recife</strong>
+                    <span>Pernambuco</span>
+                </footer>
+            </aside>
+
+            <Map
+                center={[-23.5983069, -46.6415725]}
+                zoom={15}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </Map>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF"/>
+            </Link>
+        </div>
+    );
 }
 
 export default OrphanagesMap;
